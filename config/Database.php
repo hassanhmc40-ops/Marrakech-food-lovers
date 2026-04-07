8b00ef434c87459b503a16702ecae97c3f954095
najwa13 <najwafathi05@hotmail.com>
2026-04-07 12:37:08 +0100

    add Database class with PDO connection

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,28 @@
-hsbjdkndln
+<?php
+class Database{
+    private $host = "localhost";
+    private $dbname = "marrakech-food-lovers";
+    private $username = "root";
+    private $password = '';
+    private $conn;
+
+    public function getConnection(){
+       $this->conn = null;
+
+       try{
+
+       $this->conn = new PDO ("mysql:host=" . $this->host . ",dbname=". $this->dbname , $this->username , $this->password);
+       $this->conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+       $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+       }catch(PDOException $e){
+        die("connection failed:" . $e->getMessage());
+       }
+       return $this->conn;
+    }
+    public static function connect(){
+        $db = new self();
+        return $db->getConnection();
+    }
+}
+
+?>
