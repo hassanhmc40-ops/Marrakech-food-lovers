b67d8072b7814095d71133a33d63dda0f285f2e9
hassanhmc40-ops <hassanhmc40@gmail.com>
2026-04-08 16:37:01 +0200

    add the password confirmation

--- views/auth/register.php
+++ views/auth/register.php
@@ -12,6 +12,8 @@
 
     <label for="password">Password</label>
     <input type="password" name="password" id="password" required>
+    <label for="confirm_password">Confirm Password</label>
+    <input type="password" name="confirm_password" id="confirm_password" required>
 
     <button type="submit">Register</button>
 
